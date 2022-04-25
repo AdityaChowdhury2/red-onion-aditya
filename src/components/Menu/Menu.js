bb4933796f9f5df1bcad7c5af94233814c33b676
Aditya Chowdhury <adityac5102@gmail.com>
2022-04-25 22:28:33 +0600

    menu done

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,27 +1,12 @@
-import { Typography } from '@mui/material';
 import React from 'react';
-
+import { Button } from 'react-bootstrap';
+import './menu.css'
 const Menu = () => {
-    const styles = theme => ({
-        button: {
-            color: 'black',
-            cursor: 'pointer',
-            '&:hover': {
-                color: 'red'
-            }
-        }
-    })
     return (
         <div className="mt-5 d-flex justify-content-center">
-            <Typography component='p' className={styles.button}>
-                Breakfast
-            </Typography>
-            <Typography component='p'>
-                Lunch
-            </Typography>
-            <Typography component='p'>
-                Dinner
-            </Typography>
+            <Button variant="light" className='customBtn customBtn-active'>Breakfast</Button>
+            <Button variant="light" className='customBtn'>Lunch</Button>
+            <Button variant="light" className='customBtn'>Dinner</Button>
             {/* <Button className={styles} variant="text">Breakfast</Button>
             <Button className={styles} variant="text">Lunch</Button>
             <Button className={styles} variant="text">Dinner</Button> */}
